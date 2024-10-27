9f84cc12295d64e117ec324bb7138d599b36a328
yuehchen-tsou <50720457+yuehchen-tsou@users.noreply.github.com>
2024-10-27 14:58:57 -0700

    Update LoginActivity.java

--- app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
@@ -11,20 +11,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
- //   FirebaseAuth mAuth;
+    FirebaseAuth mAuth;
     TextView txt_SignUp;
     TextInputEditText user_name, pass_word;
     ProgressBar signIn_bar;
@@ -41,7 +41,7 @@ public class LoginActivity extends AppCompatActivity {
         btn_signIn=findViewById(R.id.btnSignIn);
 
         txt_SignUp = findViewById(R.id.txtSignUp);
-//        mAuth=FirebaseAuth.getInstance();
+        mAuth=FirebaseAuth.getInstance();
 
         txt_SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,26 +55,34 @@ public class LoginActivity extends AppCompatActivity {
         btn_signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String SignInUsername = String.valueOf(user_name.getText().toString());
-                String SignInPassword= String.valueOf(pass_word.getText().toString().trim());
-                if(SignInUsername.isEmpty())
+                String email = String.valueOf(user_name.getText().toString());
+                String password= String.valueOf(pass_word.getText().toString().trim());
+                if(email.isEmpty())
                 {
                     user_name.setError("Username is empty");
                     user_name.requestFocus();
                     return;
                 }
-                if(SignInPassword.isEmpty())
+                if(password.isEmpty())
                 {
                     pass_word.setError("Password is empty");
                     pass_word.requestFocus();
                     return;
                 }
-                if(SignInPassword.length()<6)
-                {
-                    pass_word.setError("Length of password is more than 6");
-                    pass_word.requestFocus();
-                    return;
-                }
+
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                    if(task.isSuccessful())
+                    {
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this,
+                                "Please Check Your login Credentials",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                });
             }
 
         });
