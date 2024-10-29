cd02c43994d5ffe974a168a91cb6a0f6eb394beb
GiovZa <zavalzagiovanni@gmail.com>
2024-10-29 10:58:34 -0500

    Added error messages, password security, comments, and debugged team issue with access to Firebase -> Login and SignUp pages

--- app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
@@ -22,10 +22,14 @@ import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+// Login Page with Firebase Authentication and signup page link
 public class LoginActivity extends AppCompatActivity {
 
+    // Firebase implementation for user authentication
     FirebaseAuth mAuth;
-    TextView txt_SignUp;
+
+    // UI
+    TextView txt_SignUp, loginErrorText;
     TextInputEditText user_name, pass_word;
     ProgressBar signIn_bar;
     MaterialButton btn_signIn;
@@ -35,14 +39,17 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Initialize
         user_name=findViewById(R.id.SignInUsername);
         pass_word=findViewById(R.id.SignInPassword);
         signIn_bar=findViewById(R.id.SignInBar);
         btn_signIn=findViewById(R.id.btnSignIn);
+        loginErrorText = findViewById(R.id.loginErrorText);
 
         txt_SignUp = findViewById(R.id.txtSignUp);
         mAuth=FirebaseAuth.getInstance();
 
+        //  "Sign Up" text clicked
         txt_SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,17 +59,25 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        //  "Sign In" button clicked
         btn_signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Retrieve user input from the username and password fields
                 String email = String.valueOf(user_name.getText().toString());
                 String password= String.valueOf(pass_word.getText().toString().trim());
+
+                // Hide the error message text
+                loginErrorText.setVisibility(View.GONE);
+
+                // username empty guard clause
                 if(email.isEmpty())
                 {
                     user_name.setError("Username is empty");
                     user_name.requestFocus();
                     return;
                 }
+                // password empty guard clause
                 if(password.isEmpty())
                 {
                     pass_word.setError("Password is empty");
@@ -70,13 +85,18 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
 
+                // Attempt to sign in using Firebase authentication
                 mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
                     if(task.isSuccessful())
                     {
+                        // If login is successful, navigate to MainActivity
                         startActivity(new Intent(LoginActivity.this, MainActivity.class));
                     }
                     else
                     {
+                        // If login fails, display error message
+                        loginErrorText.setText("Email or password is invalid");
+                        loginErrorText.setVisibility(View.VISIBLE);
                         Toast.makeText(LoginActivity.this,
                                 "Please Check Your login Credentials",
                                 Toast.LENGTH_SHORT).show();
--- app/src/main/java/edu/uiuc/cs427app/SignUpActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/SignUpActivity.java
@@ -20,6 +20,9 @@ import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import android.util.Log;
+import java.util.regex.Pattern;
+
 public class SignUpActivity extends AppCompatActivity {
 
     TextView txt_SignIn;
@@ -28,19 +31,23 @@ public class SignUpActivity extends AppCompatActivity {
     MaterialButton btn_signUp;
     FirebaseAuth mAuth;
 
+    // Password pattern (at least one digit, one lower case, one upper case, one special character, and 6-12 characters in length)
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile(
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{6,12}$"
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-
-        user_name=(TextInputEditText) findViewById(R.id.EditEmail);
-        e_mail=findViewById(R.id.SignUpEmail);
-        pass_word=(TextInputEditText) findViewById(R.id.SignUpPassword);
-        confirm_password=findViewById(R.id.ConfirmPassword);
-        signUp_bar=findViewById(R.id.SignUpBar);
-        btn_signUp=findViewById(R.id.btnSignUp);
-        mAuth=FirebaseAuth.getInstance();
+        user_name = findViewById(R.id.EditEmail);
+        e_mail = findViewById(R.id.SignUpEmail);
+        pass_word = findViewById(R.id.SignUpPassword);
+        confirm_password = findViewById(R.id.ConfirmPassword);
+        signUp_bar = findViewById(R.id.SignUpBar);
+        btn_signUp = findViewById(R.id.btnSignUp);
+        mAuth = FirebaseAuth.getInstance();
         txt_SignIn = findViewById(R.id.txtSignIn);
 
         txt_SignIn.setOnClickListener(new View.OnClickListener() {
@@ -56,60 +63,60 @@ public class SignUpActivity extends AppCompatActivity {
         btn_signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = user_name.getText().toString().trim();
-                String password= pass_word.getText().toString().trim();
-                String ConPassword= confirm_password.getText().toString().trim();
-                String E_mail= e_mail.getText().toString().trim();
-                if(email.isEmpty())
+                String username = user_name.getText().toString().trim();
+                String password = pass_word.getText().toString().trim();
+                String confirmPassword = confirm_password.getText().toString().trim();
+                String email = e_mail.getText().toString().trim();
+
+                if (username.isEmpty())
                 {
                     user_name.setError("Username is empty");
                     user_name.requestFocus();
                     return;
                 }
-                if(!Patterns.EMAIL_ADDRESS.matcher(E_mail).matches())
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
                 {
-                    e_mail.setError("Enter the valid email address");
+                    e_mail.setError("Enter a valid email address");
                     e_mail.requestFocus();
                     return;
                 }
-                if(password.isEmpty())
+
+                if (!validatePassword(password))
                 {
-                    pass_word.setError("Enter the password");
+                    pass_word.setError("Password must contain: 6-12 characters, at least one digit, one upper case, one lower case, and one special character.");
                     pass_word.requestFocus();
                     return;
                 }
-                if(password.length()<6)
-                {
-                    pass_word.setError("Length of the password should be more than 6");
-                    pass_word.requestFocus();
-                    return;
-                }
-                if(!(password.equals(ConPassword)))
+
+                if (!password.equals(confirmPassword))
                 {
                     confirm_password.setError("Password and Confirm password are different.");
                     confirm_password.requestFocus();
                     return;
                 }
 
-
-                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful())
                         {
-                            Toast.makeText(SignUpActivity.this,"You are successfully SigUp", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignUpActivity.this, "You are successfully Signed Up", Toast.LENGTH_SHORT).show();
                         }
                         else
                         {
-                            Toast.makeText(SignUpActivity.this,"SignUp Failed" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignUpActivity.this, "SignUp Failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            // Logged error for debugging purposes
+//                            Log.e("SignUpActivity", "SignUp Failed: " + task.getException().getMessage());
                         }
                     }
                 });
             }
         });
-
-
     }
 
+    // Uses regex to check for numbers, special, upper, and lower chars
+    private boolean validatePassword(String password) {
+        return PASSWORD_PATTERN.matcher(password).matches();
+    }
 }
-
