438f9b61a37241003f335698cc9ca732173cc7d9
yuehchen-tsou <50720457+yuehchen-tsou@users.noreply.github.com>
2024-10-31 11:20:06 -0700

    Update DetailsActivity.java

--- app/src/main/java/edu/uiuc/cs427app/DetailsActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/DetailsActivity.java
@@ -9,13 +9,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+//firebase authentication
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DetailsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser mUser = mAuth.getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        
+        // get email from firebase and display titl: Team 33-"username"
+        mAuth=FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        String user_name = mUser.getEmail();
+        String username = user_name.substring(0, user_name.indexOf("@"));
+        setTitle(getString(R.string.app_name) + "-" + username);
 
         // Process the Intent payload that has opened this Activity and show the information accordingly
         String cityName = getIntent().getStringExtra("city").toString();
