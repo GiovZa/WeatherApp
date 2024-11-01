a3c6a8320644896141af164e3ac025514d9c8536
antoinelavoie67 <antoinelavoie67@gmail.com>
2024-11-01 15:55:36 -0700

    adding theme to ActivityDetails

--- app/src/main/java/edu/uiuc/cs427app/DetailsActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/DetailsActivity.java
@@ -21,19 +21,29 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
-        
-        // get email from firebase and display titl: Team 33-"username"
-        mAuth=FirebaseAuth.getInstance();
-        mUser = mAuth.getCurrentUser();
+
         String user_name = mUser.getEmail();
         String username = user_name.substring(0, user_name.indexOf("@"));
+
+        // Load the theme before setting the content view
+        ThemeManager.loadTheme(username, this, new ThemeManager.ThemeLoadCallback() {
+            @Override
+            public void onThemeLoaded() {
+                // Once the theme is applied, set the content view
+                setContentView(R.layout.activity_details);
+                initializeUI(username);
+            }
+        });
+    }
+
+    private void initializeUI(String username) {
+        // get email from firebase and display titl: Team 33-"username"
         setTitle(getString(R.string.app_name) + "-" + username);
 
         // Process the Intent payload that has opened this Activity and show the information accordingly
-        String cityName = getIntent().getStringExtra("city").toString();
-        String welcome = "Welcome to the "+cityName;
-        String cityWeatherInfo = "Detailed information about the weather of "+cityName;
+        String cityName = getIntent().getStringExtra("city");
+        String welcome = "Welcome to " + cityName;
+        String cityWeatherInfo = "Detailed information about the weather of " + cityName;
 
         // Initializing the GUI elements
         TextView welcomeMessage = findViewById(R.id.welcomeText);
@@ -41,11 +51,10 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
 
         welcomeMessage.setText(welcome);
         cityInfoMessage.setText(cityWeatherInfo);
-        // Get the weather information from a Service that connects to a weather server and show the results
 
+        // Get the weather information from a Service that connects to a weather server and show the results
         Button buttonMap = findViewById(R.id.mapButton);
         buttonMap.setOnClickListener(this);
-
     }
 
     @Override
