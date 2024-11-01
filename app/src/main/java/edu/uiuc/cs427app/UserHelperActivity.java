9e93a0d3fd8d5aa05233df1ee60a8acfa9ee07bb
yuehchen-tsou <50720457+yuehchen-tsou@users.noreply.github.com>
2024-10-31 23:15:58 -0700

    Add files via upload

---
+++ app/src/main/java/edu/uiuc/cs427app/UserHelperActivity.java
@@ -0,0 +1,46 @@
+package edu.uiuc.cs427app;
+
+public class UserHelperActivity {
+
+    String username, password, ui, cities;
+
+    public UserHelperActivity() {
+    }
+
+    public UserHelperActivity(String username, String password, String ui, String cities) {
+        this.username = username;
+        this.password = password;
+        this.ui = ui;
+        this.cities = cities;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getUI() {
+        return ui;
+    }
+
+    public void setUI(String ui) {
+        this.ui = ui;
+    }
+    public String getCities() {
+        return cities;
+    }
+
+    public void setCities(String cities) {
+        this.cities = cities;
+    }
+}
